3cd093f61350c96a01306cd6ccfbfef8f1def3f1
Beth <bethwambuimuniu@gmail.com>
2018-02-27 05:21:29 +0300

    feature(ft-consume-api):Add root file for the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import store from './store/reduxstore'
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import  RegisterContainer from './components/containers/RegisterContainer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Home extends Component{
+  render(){
+    return(
+      <div>
+        <RegisterContainer />
+      </div>
+    );
+  }
+}
+
+
+
+
+ReactDOM.render( <Provider store={store}>
+  <Home />
+  </Provider>
+  , document.getElementById('root'));
