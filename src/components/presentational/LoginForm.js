1d2dc04c4b181b8ee97548882ce6fe93fee9b7a5
betsybeth <bethwambuimuniu@gmail.com>
2018-03-18 20:11:35 +0300

    Fix[ft-create-component]:Fix react props errors

--- src/components/presentational/LoginForm.js
+++ src/components/presentational/LoginForm.js
@@ -10,6 +10,7 @@ const LoginForm = (props) =>{
           <input  className="form-control" type="email" name="email" value={props.email} onChange={props.onSignInChange} placeholder="username" autoFocus="True"/>
           <label>Password</label><br/>
           <input  className="form-control" type="password" name="password"  value={props.password} onChange={props.onSignInChange} placeholder="password" required/>
+          <p><i>New User?</i><a href="/register" onClick={props.onLoginClick}>Sign Up</a></p>
           <button type="submit"  className="btn btn-lg btn-success btn-block">Sign In</button>
       </form>
     );
