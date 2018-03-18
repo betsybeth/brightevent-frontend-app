c38070a6b0ad0cf35d005826be7cab1d42a42e7d
betsybeth <bethwambuimuniu@gmail.com>
2018-03-18 19:55:33 +0300

    Fix[ft-create-smart-components]:fix typeerror

--- src/actions/EventActions.js
+++ src/actions/EventActions.js
@@ -2,9 +2,8 @@ import { actionTypes } from '../constants/actionTypes';
 import instance from '../axiosConfigs/config';
 
 export const createEvent = eventsDetails => ({
-  type:actionTypes.CREATE_EVENT,
-  payload:instance.post('/create_event'),
-
+  type: actionTypes.CREATE_EVENT,
+  payload: instance.post('/create_event', eventsDetails),
 })
 export const PUBLIC_EVENTS = (limit=4 ,page=1)=> ({
   type:actionTypes.PUBLIC_EVENTS,
