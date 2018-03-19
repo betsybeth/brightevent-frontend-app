afeb98069897b2c550af09a37c68dd49597ff310
betsybeth <bethwambuimuniu@gmail.com>
2018-03-19 13:44:38 +0300

    feature[ft-create-redux-tests]:Add search bar tests

---
+++ src/__test__/presentationalTests/Searchbar.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { expect } from 'chai';
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import { sinon } from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
+import SearchBar from '../../components/presentational/SearchBar'
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const props = {
+  eventInfo:{
+    name:'testname',
+  }
+};
+
+
+describe('<SearchBar', () => {
+  it('render one div', () => {
+    const wrapper = shallow(<SearchBar {...props} />);
+    expect(wrapper.find('div')).to.have.length(1);
+  });
+  it('has one form ', () => {
+    const wrapper = shallow(<SearchBar {...props}/>);
+    expect(wrapper.find('form')).to.have.length(1);
+  });
+  it('has one input', () => {
+    const wrapper = shallow(<SearchBar {...props}/>);
+    expect(wrapper.find('input')).to.have.length(1);
+  });
+});
