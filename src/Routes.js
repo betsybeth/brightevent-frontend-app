63948d72e1030df877a3e28181b78d60a8eaa19b
betsybeth <bethwambuimuniu@gmail.com>
2018-05-19 10:39:57 +0300

    [feature]:Add react routing

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './components/containers/Login';
 import Register from './components/containers/Register';
-import ForgotPasswordForm from './components/presentational/ForgotPasswordForm';
+import ChangePasswordForm from './components/presentational/ChangePasswordForm';
 import Dashboard from './components/containers/Dashboard';
 import EditEventForm from './components/presentational/EditEventForm';
 import EventDetails from './components/containers/EventDetails';
@@ -16,7 +16,7 @@ const Routes = () => (
     <Route path="/register" component={Register} />
     <Route path="/dashboard" component={Dashboard} />
     <Route path="/edit-event" component={EditEventForm} />
-    <Route path="/forgot-password" component={ForgotPasswordForm} />
+    <Route path="/change-password" component={ChangePasswordForm} />
     <Route path="/eventDetails/:id/add-rsvp/" component={RsvpForm}/>
     <Route path="/eventDetails/:id" component={EventDetails} />
   </Switch>
